a4ee83398ab00980be975e711dcbfe197130829b
Kenny Luong <luong97@hawaii.edu>
2014-04-07 20:20:31 -1000

    Added intial program

---
+++ vnc-manage.py
@@ -0,0 +1,14 @@
+# ===============================================
+#
+# vncmanage.py
+# 
+# Author: Kenny Luong
+#
+# Makes your life with vnc a little easier and more
+# secure.
+#
+# ================================================
+
+import argparse
+
+parser = argparse.ArgumentParser(description='vncmanager tool')
