3d36332f3871c0807d3efd7fdd723d66249ef01b
Kenny Luong <luong97@hawaii.edu>
2014-04-27 23:27:35 -1000

    Fixed vnc tunnel to accept vnc screen numbers

--- vnc-tunnel.sh
+++ vnc-tunnel.sh
@@ -11,7 +11,8 @@
 # =======================================================
 
 ssh_alias=$1
-port=$2
+screen=$2
+port=$((5900+$screen))
 # ssh -L 5901:localhost:5901 -N -f rdr -c arcfour,blowfish-cbc -XC
 ./create_tunnel.sh $ssh_alias $port
-vncviewer localhost:1
+vncviewer localhost:$screen
